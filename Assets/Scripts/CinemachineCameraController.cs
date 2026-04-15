99204f83c512e463631319ac53b7f555375fd339
wassup-999 <sebasquispeperez@gmail.com>
2026-04-14 19:00:04 -0500

    Avance

--- Assets/Scripts/CinemachineCameraController.cs
+++ Assets/Scripts/CinemachineCameraController.cs
@@ -1,64 +1,99 @@
-using UnityEngine;
 using Sirenix.OdinInspector;
 using Unity.Cinemachine;
+using UnityEditor.Rendering;
+using UnityEngine;
 using UnityEngine.Splines;
 public enum Cameras
 {
     Camera_1,
     Camera_2,
     Camera_3, 
-    Camera_4
+    Camera_4,
+    CameraDefault,
 }
 public class CinemachineCameraController : MonoBehaviour
 {
     public CinemachineCamera camA;
     public CinemachineCamera camB;
+    public CinemachineCamera camC;
+    public CinemachineCamera camD;
     public Cameras Numcam;
-    public float switchTime;
     public float counter;
+    public int CamerasCount;
     void Start()
     {
-        
+
     }
 
-    
+
     void Update()
     {
         SwitchCamera();
     }
-    
+
     public void SwitchCamera()
     {
+        
         counter += Time.deltaTime;
-        if (camB.Priority > camA.Priority)
-        {         
-            camB.Priority = 10;
-            camA.Priority = 20;
-            
-        }
-        if (counter >= switchTime)
-        {               
-             camB.Priority = 20;
-             camA.Priority = 10;          
-        }      
-        /*
-        else
-        {
-            camB.Priority = 20;
-            camA.Priority = 10;
-        }  */
+        NextCam();
     }
-    /*
-    public void ChangeCam()
+    
+    public void NextCam()
     {
-        switch (Cameras)
+        switch (Numcam)
         {
-            case.Camera_1:
-            {
-                
-            }
+            case Cameras.Camera_1:
+                if (counter>=10)
+                {
+                    Numcam= Cameras.Camera_2;
+                    camA.Priority = 20;                  
+                }
+                break;
+
+            case Cameras.Camera_2:
+                if(counter>=20)
+                {
+                    Numcam = Cameras.Camera_3;
+                    camB.Priority = 30;
+                    CamerasCount = 2;
+                }
+                break;
+
+            case Cameras.Camera_3:
+                if(counter>=30)
+                {
+                    Numcam = Cameras.Camera_4;
+                    camC.Priority = 40;
+                    CamerasCount = 3;
+                }
+                break;
+
+            case Cameras.Camera_4:
+                if(counter>=40)
+                {
+
+                    camD.Priority = 50;
+                    Numcam = Cameras.CameraDefault;
+                    CamerasCount = 4;
+                }
+                break;
+            case Cameras.CameraDefault:
+                if(counter>=50)
+                {
+                    ResetCam();                   
+                    Numcam = Cameras.Camera_1;                  
+                } 
+                break;
         }
     }
-    */
-
+    
+    public void ResetCam()
+    {
+        camA.Priority = 10;
+        camB.Priority = 10;
+        camC.Priority = 10;
+        camD.Priority = 10;
+        counter = 0;
+        CamerasCount = 1;
+    }
 }
