e864536a249189d4a557ab9f3e1634b2eb10f8c2
wassup-999 <sebasquispeperez@gmail.com>
2026-04-18 12:46:49 -0500

    Scene and Gm changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,29 +18,5 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-    }
-    /*
-    public CinemachineCameraController camA;
-    public CinemachineCameraController camB;
-
-
-    [Button("Transition")]
-    public void transition()
-    {
-        if (camA.Priority > camB.Priority)
-        {
-            camA.Priority = 0;
-            camB.Priority = 1;
-        }
-        else
-        {
-            camA.Priority = 1;
-            camB.Priority = 0;
-        }
-    }
-    public void OnCameraFinished()
-    {
-        Debug.Log("Camera transition finished!");
-    }
-    */
+    }   
 }
